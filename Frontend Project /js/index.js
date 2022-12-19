327a0ead961b9fc3ca82d5b6105161c68b96207a
Bakdet <120990006+Bakdet@users.noreply.github.com>
2022-12-19 11:33:26 +0600

    Add files via upload

---
+++ Frontend Project /js/index.js
@@ -0,0 +1,122 @@
+//Navbar button
+const bar = document.querySelector(".fa-bars");
+const menu = document.querySelector(".menu");
+
+bar.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+});
+
+//Scrooll Animated
+const bgImageEl = document.getElementById("bg-image");
+
+window.addEventListener("scroll", () => {
+  updateImage();
+});
+
+function updateImage() {
+  bgImageEl.style.opacity = 1 - window.pageYOffset / 900;
+  bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+}
+//Corusel
+const nextEl = document.querySelector(".next");
+
+const prevEl = document.querySelector(".prev");
+
+const imgsEl = document.querySelectorAll("img");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+let currentImg = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if(checkRequired([email, password, ])){
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    
+  }
+
+});
